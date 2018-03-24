3c08bd3e10dc7996f58de47e27a95bdf0ce1140c
Khairil <sangbima.net@gmail.com>
2018-03-24 16:47:05 +0700

    44 hooking up to firebase

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -4,7 +4,7 @@
     <input type="text" v-model="search" placeholder="Search blogs"/>
     <div v-for="(blog, index) in filteredBlogs" v-bind:key="index" class="single-blog">
       <router-link v-bind:to="'/blog/' + blog.id"><h2>{{ blog.title | to-uppercase }}</h2></router-link>
-      <article>{{ blog.body | snippet }}</article>
+      <article>{{ blog.content | snippet }}</article>
     </div>
   </div>
 </template>
@@ -23,8 +23,15 @@ export default {
 
   },
   created () {
-    this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data) {
-      this.blogs = data.body.slice(0, 10) // get 10 element from data
+    this.$http.get('https://my-vue-blog-project.firebaseio.com/posts.json').then(function(data) {
+     return data.json()
+    }).then(function(data) {
+      var blogsArray = []
+      for (let key in data) {
+        data[key].id = key
+        blogsArray.push(data[key])
+      }
+      this.blogs = blogsArray
     })
   },
   computed: {
--- src/components/singleBlog.vue
+++ src/components/singleBlog.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="single-blog">
     <h1>{{ blog.title }}</h1>
-    <article>{{ blog.body }}</article>
+    <article>{{ blog.content }}</article>
+    <p>Author {{ blog.author }}</p>
+    <ul>
+      <li v-for="(category, index) in blog.categories" v-bind:key="index">{{ category }}</li>
+    </ul>
   </div>
 </template>
 
@@ -16,9 +20,10 @@ export default {
     }
   },
   created () {
-    this.$http.get('https://jsonplaceholder.typicode.com/posts/' + this.id).then(function(data) {
-      console.log(data)
-      this.blog = data.body
+    this.$http.get('https://my-vue-blog-project.firebaseio.com/posts/' + this.id + '.json').then(function(data) {
+      return data.json()
+    }).then(function(data){
+      this.blog = data
     })
   }
 }
