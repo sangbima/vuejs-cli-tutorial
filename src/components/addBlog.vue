7e13a479fd6a2c6a04167543e605e591efecc80e
Khairil <sangbima.net@gmail.com>
2018-03-24 16:28:22 +0700

    43 posting to firebase

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -58,11 +58,7 @@ export default {
   },
   methods: {
     post: function () {
-      this.$http.post('https://jsonplaceholder.typicode.com/posts', {
-        title: this.blog.title,
-        body: this.blog.content,
-        userId: 1
-      }).then(function(data) {
+      this.$http.post('https://my-vue-blog-project.firebaseio.com/posts.json', this.blog).then(function(data) {
         console.log(data)
         this.submitted = true
       })
